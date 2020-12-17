0e54e618139617dd76604efc124ee4a81c4b7152
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2020-12-17 10:25:02 +0300

    Updated date.py

--- examples/date.py
+++ examples/date.py
@@ -1,7 +1,7 @@
 import news_python
 
 news = news_python.Global(key="API-KEY")
-news_content = news.get_by_date(query="bitcoin", frm="2020 11 17")
+news_content = news.get_by_date(query="bitcoin", frm="2020 11 17", to="2020 12 17")
 
 print(f"{news_content.title}\n"
       f"{news_content.url}\n"
