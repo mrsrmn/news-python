8ab52d60ce852d8aeed3c86ac0907dd12382c046
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2020-12-17 10:26:52 +0300

    Changed the examples

--- examples/date.py
+++ examples/date.py
@@ -3,6 +3,6 @@ import news_python
 news = news_python.Global(key="API-KEY")
 news_content = news.get_by_date(query="bitcoin", frm="2020 11 17", to="2020 12 17")
 
-print(f"{news_content.title}\n"
-      f"{news_content.url}\n"
-      f"{news_content.author}")
+print(f"Title: {news_content.title}\n"
+      f"URL: {news_content.url}\n"
+      f"Author: {news_content.author}")
--- examples/news.py
+++ examples/news.py
@@ -3,6 +3,7 @@ import news_python
 news = news_python.Global(key="API-KEY")
 news_content = news.get_news(query="bitcoin", source="cnn")
 
-print(f"{news_content.title}\n"
-      f"{news_content.url}\n"
-      f"{news_content.author}")
+print(f"Title: {news_content.title}\n"
+      f"URL: {news_content.url}\n"
+      f"Author: {news_content.author}")
+
