5d7c1e96890baa6b54569cf533f352498990e86f
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2020-12-17 10:32:48 +0300

    Changed the examples

--- examples/news.py
+++ examples/news.py
@@ -1,9 +1,8 @@
 import news_python
 
-news = news_python.Global(key="API-KEY")
-news_content = news.get_news(query="bitcoin", source="cnn")
+news = news_python.Country(key="API-KEY")
+news_content = news.get_news(country="us", category="tech")
 
 print(f"Title: {news_content.title}\n"
       f"URL: {news_content.url}\n"
       f"Author: {news_content.author}")
-
