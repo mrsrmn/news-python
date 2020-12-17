82ddcd45559b84c9b26c88a8ca9e64cd74146d76
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2020-12-17 10:03:20 +0300

    Added __source__

--- news_python/__init__.py
+++ news_python/__init__.py
@@ -4,3 +4,4 @@ from .global_news import Global
 __version__ = "1.0.0"
 __author__ = "MakufonSkifto"
 __license__ = "GNU General Public License v3.0"
+__source__ = "https://github.com/MakufonSkifto/news-python"
